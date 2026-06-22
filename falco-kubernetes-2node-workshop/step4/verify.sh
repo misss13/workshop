7fa03d8c55bb2c81886d030305f9252e5e71235e
Balalaika <37311848+misss13@users.noreply.github.com>
2026-06-22 22:13:27 +0200

    eq

--- falco-kubernetes-2node-workshop/step4/verify.sh
+++ falco-kubernetes-2node-workshop/step4/verify.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-test "$(kubectl logs -l app.kubernetes.io/name=falco -n falco -c falco | grep Warning | grep 'Ssh keys added to authorized_keys' | wc -l)" -gt 1
+test "$(kubectl logs -l app.kubernetes.io/name=falco -n falco -c falco | grep Warning | grep 'Ssh keys added to authorized_keys' | wc -l)" -eq 1
